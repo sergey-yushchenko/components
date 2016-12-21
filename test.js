fda15daa5085ef885bf266e81d846a19a2b915af
sergey-yushchenko <sergey.yushchenko@gmail.com>
2016-12-21 14:35:37 +0300

    Тест загрузки js https

---
+++ test.js
@@ -0,0 +1,16 @@
+/**
+ * Исходные данные
+ */
+
+var a1 = [1,5,6,17,8,9],
+    a2 = [
+     {name: "Вася", spec: "PHP", salary: 5e4},
+     {name: "Аня", spec: "SEO", salary: 3e4},
+     {name: "Петя", spec: "JavaScript", salary: 1e5},
+     {name: "Саша", spec: "MySQL", salary: 4e4},
+     {name: "Женя", spec: "HTML&CSS", salary: 3e4},    
+     {name: "Паша", spec: "JavaScript", salary: 5e4}    
+    ];
+
+    // console.warn();   
+    // _.partition(a1,function(n){ return n % 2 });
