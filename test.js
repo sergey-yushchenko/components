024df3e260c8b3c022c0f1e60a10df2adaf0dfa9
sergey-yushchenko <sergey.yushchenko@gmail.com>
2016-12-22 12:06:47 +0300

    Update test.js

--- test.js
+++ test.js
@@ -12,20 +12,3 @@ var a1 = [1,5,6,17,8,9],
      {name: "Женя", spec: "HTML&CSS", salary: 3e4},    
      {name: "Паша", spec: "JavaScript", salary: 5e4}    
     ];
-
-//     console.warn('_.each');
-    $('.item-each').next().hide();
-    $('.item-each').click(function () { $(this).next().toggle(); });
-
-    _.each( a1, function (el) { $(".list-01").append("<li class='item'>" + el + "</li>") });
-
-    _.each( a2, function (el) { $(".list-02").append("<li class='item'>" + el.name + "</li>") });
-
-    _.each( a2, function (el) { $(".list-03").append(
-      '<ul class="list-04">'
-      + "<li class='item'>" + el.name + "</li>" 
-      + "<li class='item'>" + el.spec + "</li>" 
-      + "<li class='item'>" + el.salary + "</li>"
-      + '</ul>'
-      + '<hr>'
-    ) });
